0a7aff0c23319da82cbddaae7846102a880f944e
Felippe Miguel <felippe.miguel.silva@gmail.com>
2019-03-19 22:14:02 -0300

    Removendo servidor e adicionando API
    - Removendo servidor do projeto frontend
    - Adicionando API para comunicação com endpoints do servidor

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,22 +1,11 @@
-import { getAll as getAllPosts} from '../utils/api-server/posts'
-import { getAll as getAllCategories} from '../utils/api-server/categories'
+import { getPosts } from '../utils/api'
 import { receivePosts } from './posts'
-import { receiveCategories } from './categories'
 
 export function handleGetAllPosts () {
     return (dispatch) => {
-        return getAllPosts().
-            then(({ posts }) => {
+        return getPosts()
+        .then(( posts ) => {
                 dispatch(receivePosts(posts))
-            })
-    }
-}
-
-export function handleGetAllCategories () {
-    return (dispatch) => {
-        return getAllCategories().
-            then(({ categories }) => {
-                dispatch(receiveCategories(categories))
             })
     }
 }
